213ce0c57fbcfb6ef90d60c0b99138c3262543c6
Krishna Jade <krishnajade222@gmail.com>
2023-05-09 18:02:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/complaintmanagementsystem/services/ApiService.java
@@ -0,0 +1,18 @@
+package com.example.complaintmanagementsystem.services;
+
+import com.example.complaintmanagementsystem.models.User;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @FormUrlEncoded
+    @POST("sereng.php?apicall=login")
+    Call<String> login(@Field("username") String username, @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("sereng.php?apicall=read")
+    Call<User> getUser(@Field("mobile") String mobileNumber);
+}
